cdfaf1bbd7b62073455cb8f4008471a604c72325
Fayaad Abrahams <fayaad1999@gmail.com>
2025-04-25 11:38:15 +0200

    Added some buttons to replicate the actual site

--- src/index.js
+++ src/index.js
@@ -4,11 +4,11 @@ let mainSelector = 0;
 document.addEventListener("DOMContentLoaded", () => {
     homePage();
 
-    document.querySelector(".menu-btn").addEventListener("click", () => { 
+    document.querySelector(".menu-btn").addEventListener("click", () => {
         menuPage();
     });
 
-    document.querySelector(".logo-home").addEventListener("click", () => { 
+    document.querySelector(".logo-home").addEventListener("click", () => {
         homePage();
     });
 });
@@ -17,23 +17,35 @@ document.addEventListener("DOMContentLoaded", () => {
 function homePage() {
     const contentDiv = document.getElementById("content");
     const homepageContent = `
-      <h1>Fire up your BK® order!</h1>
-      <p>This content was loaded on page load using JavaScript.</p>
-        <p>We serve the best food in town!</p>
-        <p>Check out our menu for more details.</p>
-        
+    <div class="main-container"> 
+    <div class="main-heading-container">
+    <h1>Fire up your <br>BK® order!</h1>
+       
+    <button class="order-pickup-btn order">
+    <img src="./assets/motorbike.svg" class="bike-icon" alt>
+    Order Pickup
+    </button>
+    <button class="order-pickup-btn deliver">
+    <img src="./assets/cart.svg" class="bike-icon" alt>
+    Order Delivery</button>
+    </div>
+    <div class="banner-container">
+        <img class="food-banner" src="https://cdn.sanity.io/images/czqk28jt/prod_bk_us/23ade834ac1dfb5adc195c6f977540dcb2dcc1ca-2100x1050.png?w=900&q=75&fit=max&auto=format" alt="Burger King">
+    </div>
+    <p>Check out our menu for more details.</p>  
+  </.div>
     `;
     contentDiv.innerHTML = homepageContent;
-
 }
 
-function menuPage(){
+function menuPage() {
     const contentDiv = document.getElementById("content");
     const homepageContent = `
-      <h1>THIS IS THE MENU!</h1>
+      <h1>IMRA YOU GONNA BE FINE!!</h1>
       <p>This content was loaded on page load using JavaScript.</p>
         <p>We serve the best food in town!</p>
         <p>Check out our menu for more details.</p>
     `;
     contentDiv.innerHTML = homepageContent;
 }
+
